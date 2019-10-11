b51fd43627ee8d5db1612853e4ba2b5075254f16
xiaoyong mao <43345113@qq.com>
2019-10-11 18:15:01 +0800

    update

--- Program.cs
+++ Program.cs
@@ -11,20 +11,16 @@ namespace code_generator
     class Program
     {
         static string defaultSource = "source";
-
         static string defaultTarget = "target";
-
         static string cs = "cs";
-
         static string ts = "ts";
-
         static string ns;
-
         static string csOrNot;
-
         static string tsOrNot;
+        // static string appName;
+        static string[] names;
 
-        static void CreateDir()
+        static void CreateDirs()
         {
             if (!Directory.Exists(defaultTarget + '/' + cs + '/' + CSharpGenerator.MODELS))
             {
@@ -42,34 +38,139 @@ namespace code_generator
             {
                 Directory.CreateDirectory(defaultTarget + '/' + cs + '/' + CSharpGenerator.CONTROLLERS);
             }
+            if (!Directory.Exists(defaultTarget + '/' + cs + '/' + CSharpGenerator.COMMON))
+            {
+                Directory.CreateDirectory(defaultTarget + '/' + cs + '/' + CSharpGenerator.COMMON);
+            }
+            if (!Directory.Exists(defaultTarget + '/' + cs + '/' + CSharpGenerator.SERVICES))
+            {
+                Directory.CreateDirectory(defaultTarget + '/' + cs + '/' + CSharpGenerator.SERVICES);
+            }
+            if (!Directory.Exists(defaultTarget + '/' + cs + '/' + CSharpGenerator.SERVICES + '/' + CSharpGenerator.INTERFACES))
+            {
+                Directory.CreateDirectory(defaultTarget + '/' + cs + '/' + CSharpGenerator.SERVICES + '/' + CSharpGenerator.INTERFACES);
+            }
+            if (!Directory.Exists(defaultTarget + '/' + cs + '/' + CSharpGenerator.APPCONTEXT))
+            {
+                Directory.CreateDirectory(defaultTarget + '/' + cs + '/' + CSharpGenerator.APPCONTEXT);
+            }
+            if (!Directory.Exists(defaultTarget + '/' + cs + '/' + CSharpGenerator.VIEWMODELS))
+            {
+                Directory.CreateDirectory(defaultTarget + '/' + cs + '/' + CSharpGenerator.VIEWMODELS);
+            }
         }
 
         static void Interact()
         {
             Console.WriteLine("欢迎使用代码生成工具！");
+            // while (string.IsNullOrEmpty(appName))
+            // {
+            //     System.Console.WriteLine("请输入app的名称");
+            //     appName = Console.ReadLine();
+            // }
             while (string.IsNullOrEmpty(ns))
             {
                 System.Console.WriteLine("请输入C#代码的命名空间");
                 ns = Console.ReadLine();
             }
-            while (string.IsNullOrEmpty(csOrNot) || csOrNot.Trim() != "Y" && csOrNot.Trim() != "n")
+            // while (string.IsNullOrEmpty(csOrNot) || csOrNot.Trim() != "Y" && csOrNot.Trim() != "n")
+            // {
+            //     System.Console.WriteLine("是否要生成C#代码[Y/n]");
+            //     csOrNot = Console.ReadLine();
+            // }
+            // while (string.IsNullOrEmpty(tsOrNot) || tsOrNot.Trim() != "Y" && tsOrNot.Trim() != "n")
+            // {
+            //     System.Console.WriteLine("是否要生成Typescript代码[Y/n]");
+            //     tsOrNot = Console.ReadLine();
+            // }
+        }
+
+        static void CreateBaseFiles()
+        {
+            CreateGuidEntityFile();
+            CreateBaseRepositoryFile();
+            CreateBaseRepositoryInterfaceFlie();
+            CreateAppContextFile();
+            CreateAutoMapperConfigFile();
+        }
+
+        static void CreateFile(string fileName, string fileContent)
+        {
+            if (File.Exists(fileName))
             {
-                System.Console.WriteLine("是否要生成C#代码[Y/n]");
-                csOrNot = Console.ReadLine();
+                File.Delete(fileName);
             }
-            while (string.IsNullOrEmpty(tsOrNot) || tsOrNot.Trim() != "Y" && tsOrNot.Trim() != "n")
+            using (var streamWriter = File.CreateText(fileName))
             {
-                System.Console.WriteLine("是否要生成Typescript代码[Y/n]");
-                tsOrNot = Console.ReadLine();
+                streamWriter.Write(fileContent);
             }
         }
-        
-        static void Main(string[] args)
+
+        static void CreateGuidEntityFile()
         {
-            //创建目标目录
-            CreateDir();
-            //人机交互
-            Interact();
+            string fileName = string.Format("{0}/{1}/{2}/GuidEntity.cs", defaultTarget, cs, CSharpGenerator.COMMON);
+            string fileContent = CSharpGenerator.GenerateGuidEntityText(ns);
+            CreateFile(fileName, fileContent);
+        }
+
+        static void CreateBaseRepositoryFile()
+        {
+            string fileName = string.Format("{0}/{1}/{2}/BaseRepository.cs", defaultTarget, cs, CSharpGenerator.REPOSITORIES);
+            string fileContent = CSharpGenerator.GenerateBaseRepositoryText(ns);
+            CreateFile(fileName, fileContent);
+        }
+
+        static void CreateBaseRepositoryInterfaceFlie()
+        {
+            string fileName = string.Format("{0}/{1}/{2}/{3}/IBaseRepository.cs", defaultTarget, cs, CSharpGenerator.REPOSITORIES, CSharpGenerator.INTERFACES);
+            string fileContent = CSharpGenerator.GenerateBaseRepositoryInterfaceText(ns);
+            CreateFile(fileName, fileContent);
+        }
+
+        static void CreateAppContextFile()
+        {
+            string fileName = string.Format("{0}/{1}/{2}/{3}.cs", defaultTarget, cs, CSharpGenerator.APPCONTEXT, CSharpGenerator.APPCONTEXT);
+            string fileContent = CSharpGenerator.GenerateContextText(names, ns);
+            CreateFile(fileName, fileContent);
+        }
+
+
+        static void CreateAutoMapperConfigFile()
+        {
+            string fileName = string.Format("{0}/{1}/AutoMapperConfigs.cs", defaultTarget, cs);
+            string fileContent = CSharpGenerator.GenerateAutoMapperConfigText(names, ns);
+            CreateFile(fileName, fileContent);
+        }
+
+        static void GetNames()
+        {
+            string[] files = Directory.GetFiles(defaultSource);
+            names = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                using (var stream = File.OpenText(files[i]))
+                {
+                    var jsonText = stream.ReadToEnd();
+                    var obj = JsonConvert.DeserializeObject<ModelDeclare>(jsonText);
+                    names[i] = obj.Name;
+                }
+            }
+        }
+        static void CreateModelFile(ModelDeclare model)
+        {
+            string fileName = string.Format("{0}/{1}/{2}/{3}.cs", defaultTarget, cs, CSharpGenerator.MODELS, model.Name);
+            string fileContent = CSharpGenerator.GenerateModelText(model, ns);
+            CreateFile(fileName, fileContent);
+        }
+        static void CreateViewModelFile(ModelDeclare model)
+        {
+            string fileName = string.Format("{0}/{1}/{2}/{3}.cs", defaultTarget, cs, CSharpGenerator.VIEWMODELS, model.Name);
+            string fileContent = CSharpGenerator.GenerateViewModelText(model, ns);
+            CreateFile(fileName, fileContent);
+        }
+
+        static void CreateBusinessFiles()
+        {
             string[] files = Directory.GetFiles(defaultSource);
             foreach (var file in files)
             {
@@ -77,20 +178,25 @@ namespace code_generator
                 {
                     var jsonText = stream.ReadToEnd();
                     var obj = JsonConvert.DeserializeObject<ModelDeclare>(jsonText);
-
-                    string fileNameModel = string.Format("{0}/{1}/{2}/{3}.cs", defaultTarget, cs, CSharpGenerator.MODELS, obj.Name);
-                    if (File.Exists(fileNameModel))
-                    {
-                        File.Delete(fileNameModel);
-                    }
-                    using (var streamWriter = File.CreateText(fileNameModel))
-                    {
-                        streamWriter.Write(CSharpGenerator.GenerateModelText(obj, ns));
-                    }
-
+                    CreateModelFile(obj);
+                    CreateViewModelFile(obj);
                 }
 
             }
+        }
+
+        static void Main(string[] args)
+        {
+            //创建目标目录
+            CreateDirs();
+            //人机交互
+            Interact();
+            //获得model名字
+            GetNames();
+            //建立基础文件
+            CreateBaseFiles();
+            //建立业务文件
+            CreateBusinessFiles();
         }
     }
 }
--- utils/CSharpGenerator.cs
+++ utils/CSharpGenerator.cs
@@ -6,24 +6,283 @@ namespace code_generator.utils
     public class CSharpGenerator
     {
         public static string MODELS = "Models";
+        public static string VIEWMODELS = "ViewModels";
         public static string REPOSITORIES = "Repositories";
         public static string REPOSITORY = "Repository";
         public static string INTERFACES = "Interfaces";
+        public static string INTERFACE = "Interface";
         public static string CONTROLLERS = "Controllers";
-        public static string GenerateModelText(ModelDeclare model, string namespaceText)
+        public static string APPCONTEXT = "AppContext";
+        public static string CONTEXT = "Context";
+        public static string COMMON = "Common";
+        public static string SERVICES = "Services";
+        public static string SERVICE = "Service";
+
+        private static string GetModelText(ModelDeclare model, string namespaceText, string modelOrViewModel)
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine(string.Format("namespace {0}.{1}", namespaceText, MODELS));
-            sb.AppendLine("{");
-            sb.AppendLine(string.Format("    public class {0}", model.Name));
-            sb.AppendLine("    {");
+            sb.AppendLine(@"using System;");
+            sb.AppendLine(@"using System.Collections.Generic;");
+            sb.AppendLine(string.Format(@"using {0}.{1};", namespaceText, COMMON));
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"namespace {0}.{1}", namespaceText, modelOrViewModel));
+            sb.AppendLine(@"{");
+            sb.AppendLine(string.Format(@"    public class {0} : GuidEntity", model.Name));
+            sb.AppendLine(@"    {");
             foreach (var prop in model.ModelProperties)
             {
-                sb.AppendLine(string.Format("        public {0} {1} {{ get; set; }}", TypeConveror.ToCSharpType(prop.ValueType), prop.Name));
+                sb.AppendLine(string.Format(@"        public {0} {1} {{ get; set; }}", TypeConveror.ToCSharpType(prop.ValueType), prop.Name));
             }
             //sb.AppendLine(TypeConveror.ToCSharpType())
-            sb.AppendLine("    }");
-            sb.AppendLine("}");
+            sb.AppendLine(@"    }");
+            sb.AppendLine(@"}");
+            return sb.ToString();
+        }
+        public static string GenerateModelText(ModelDeclare model, string namespaceText)
+        {
+            return GetModelText(model, namespaceText, MODELS);
+        }
+
+        public static string GenerateViewModelText(ModelDeclare model, string namespaceText)
+        {
+            return GetModelText(model, namespaceText, VIEWMODELS);
+        }
+
+        public static string GenerateRepositoryText(ModelDeclare model, string namespaceText)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Format(@"namespace {0}.{1}", namespaceText, REPOSITORIES));
+            sb.AppendLine(@"{");
+            sb.AppendLine(@"}");
+            return sb.ToString();
+        }
+
+        public static string GenerateRepositoryInterfaceText(ModelDeclare model, string namespaceText)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"using System;");
+            sb.AppendLine(@"using System.Collections.Generic;");
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"namespace {0}.{1}.{2}", namespaceText, REPOSITORIES, INTERFACES));
+            sb.AppendLine(@"{");
+            sb.AppendLine(@"}");
+            return sb.ToString();
+        }
+
+        public static string GenerateContextText(string[] names, string namespaceText)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"using Microsoft.EntityFrameworkCore;");
+            sb.AppendLine(string.Format(@"using {0}.Models;", namespaceText));
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"namespace {0}", namespaceText));
+            sb.AppendLine(@"{");
+            sb.AppendLine(string.Format(@"    public class {0} : DbContext", APPCONTEXT));
+            sb.AppendLine(@"    {");
+            sb.AppendLine(string.Format(@"        public {0}(DbContextOptions<{0}> options)", APPCONTEXT));
+            sb.AppendLine(@"            : base(options)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine();
+            sb.AppendLine(@"        }");
+            foreach (var name in names)
+            {
+                sb.AppendLine();
+                sb.AppendLine(string.Format(@"        public DbSet<{0}> {0} {{ get; set; }}", name));
+            }
+            sb.AppendLine(@"    }");
+            sb.AppendLine(@"}");
+            return sb.ToString();
+        }
+
+        public static string GenerateBaseRepositoryInterfaceText(string namespaceText)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"using System;");
+            sb.AppendLine(@"using System.Collections.Generic;");
+            sb.AppendLine(string.Format(@"namespace {0}.{1}.{2}", namespaceText, REPOSITORIES, INTERFACES));
+            sb.AppendLine(@"{");
+            sb.AppendLine(@"    public interface IBaseRepository<T>");
+            sb.AppendLine(@"        where T : class");
+            sb.AppendLine(@"    {");
+            sb.AppendLine();
+            sb.AppendLine(@"        void Add(T model);");
+            sb.AppendLine();
+            sb.AppendLine(@"        void Add(IEnumerable<T> collection);");
+            sb.AppendLine();
+            sb.AppendLine(@"        void Update(T model);");
+            sb.AppendLine();
+            sb.AppendLine(@"        void Remove(T model);");
+            sb.AppendLine();
+            sb.AppendLine(@"        void Remove(IEnumerable<T> collection);");
+            sb.AppendLine();
+            sb.AppendLine(@"        T Get(Guid id);");
+            sb.AppendLine();
+            sb.AppendLine(@"        IEnumerable<T> Get(Func<T, bool> predicate = null);");
+            sb.AppendLine();
+            sb.AppendLine(@"    }");
+            sb.AppendLine(@"}");
+            return sb.ToString();
+        }
+
+        public static string GenerateBaseRepositoryText(string namespaceText)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"using System;");
+            sb.AppendLine(@"using System.Collections.Generic;");
+            sb.AppendLine(@"using System.Linq;");
+            sb.AppendLine(string.Format(@"using {0}.Repositories.Interfaces;", namespaceText));
+            sb.AppendLine(string.Format(@"using {0}.Common;", namespaceText));
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"namespace {0}.{1}", namespaceText, REPOSITORIES));
+            sb.AppendLine(@"{");
+            sb.AppendLine(@"    public abstract class BaseRepository<T> : IBaseRepository<T>");
+            sb.AppendLine(@"        where T : GuidEntity");
+            sb.AppendLine(@"    {");
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"        protected {0} _context;", APPCONTEXT));
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"        public BaseRepository({0} context)", APPCONTEXT));
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            this._context = context;");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public void Add(IEnumerable<T> collection)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            this._context.Set<T>().AddRange(collection);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public void Add(T model)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            this._context.Add(model);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public IEnumerable<T> Get(Func<T, bool> predicate = null)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            IEnumerable<T> collection = this._context.Set<T>();");
+            sb.AppendLine(@"            if (predicate != null)");
+            sb.AppendLine(@"            {");
+            sb.AppendLine(@"                collection = collection.Where(predicate);");
+            sb.AppendLine(@"            }");
+            sb.AppendLine(@"            return collection;");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public T Get(Guid id)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            return this._context.Set<T>().DefaultIfEmpty(null).SingleOrDefault(e => e.Id == id);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public void Remove(IEnumerable<T> collection)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            this._context.Set<T>().RemoveRange(collection);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public void Remove(T model)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            this._context.Set<T>().Remove(model);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public void Update(T model)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            this._context.Set<T>().Update(model);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"    }");
+            sb.AppendLine(@"}");
+            return sb.ToString();
+        }
+
+        public static string GenerateGuidEntityText(string namespaceText)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"using System;");
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"namespace {0}.{1}", namespaceText, COMMON));
+            sb.AppendLine(@"{");
+            sb.AppendLine(@"    public abstract class GuidEntity");
+            sb.AppendLine(@"    {");
+            sb.AppendLine(@"        int? _requestedHashCode;");
+            sb.AppendLine();
+            sb.AppendLine(@"        Guid _Id;");
+            sb.AppendLine();
+            sb.AppendLine(@"        public virtual Guid Id { get; set; }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public bool IsTransient()");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            return this.Id == Guid.Empty;");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public override bool Equals(object obj)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            if (obj == null || !(obj is GuidEntity))");
+            sb.AppendLine(@"                return false;");
+            sb.AppendLine(@"            if (Object.ReferenceEquals(this, obj))");
+            sb.AppendLine(@"                return true;");
+            sb.AppendLine(@"            GuidEntity item = (GuidEntity)obj;");
+            sb.AppendLine(@"            if (item.IsTransient() || this.IsTransient())");
+            sb.AppendLine(@"                return false;");
+            sb.AppendLine(@"            else");
+            sb.AppendLine(@"                return item.Id == this.Id;");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public override int GetHashCode()");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            if (!IsTransient())");
+            sb.AppendLine(@"            {");
+            sb.AppendLine(@"                if (!_requestedHashCode.HasValue)");
+            sb.AppendLine(@"                    _requestedHashCode = this.Id.GetHashCode() ^ 31;");
+            sb.AppendLine(@"                return _requestedHashCode.Value;");
+            sb.AppendLine(@"            }");
+            sb.AppendLine(@"            else");
+            sb.AppendLine(@"                return base.GetHashCode();");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public static bool operator ==(GuidEntity left, GuidEntity right)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            if (Object.Equals(left, null))");
+            sb.AppendLine(@"                return (Object.Equals(right, null)) ? true : false;");
+            sb.AppendLine(@"            else");
+            sb.AppendLine(@"                return left.Equals(right);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"        public static bool operator !=(GuidEntity left, GuidEntity right)");
+            sb.AppendLine(@"        {");
+            sb.AppendLine(@"            return !(left == right);");
+            sb.AppendLine(@"        }");
+            sb.AppendLine();
+            sb.AppendLine(@"    }");
+            sb.AppendLine(@"}");
+            return sb.ToString();
+        }
+
+        public static string GenerateAutoMapperConfigText(string[] names, string namespaceText)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"using AutoMapper;");
+            sb.AppendLine(@"using System;");
+            sb.AppendLine(@"using System.Collections.Generic;");
+            sb.AppendLine(@"using System.Linq;");
+            sb.AppendLine();
+            sb.AppendLine(string.Format(@"namespace {0}", namespaceText));
+            sb.AppendLine(@"{");
+            sb.AppendLine(@"    public class AutoMapperConfigs : Profile");
+            sb.AppendLine(@"    {");
+            sb.AppendLine(@"        public AutoMapperConfigs()");
+            sb.AppendLine(@"        {");
+            sb.AppendLine();
+            foreach (var name in names)
+            {
+                sb.AppendLine(string.Format(@"            #region {0} Model", name));
+                sb.AppendLine();
+                sb.AppendLine(string.Format(@"            CreateMap<{0}.{2}, {1}.{2}>();", MODELS, VIEWMODELS, name));
+                sb.AppendLine();
+                sb.AppendLine(string.Format(@"            CreateMap<{0}.{2}, {1}.{2}>();", VIEWMODELS, MODELS, name));
+                sb.AppendLine();
+                sb.AppendLine(@"            #endregion");
+                sb.AppendLine();
+            }
+            sb.AppendLine(@"        }");
+            sb.AppendLine(@"    }");
+            sb.AppendLine(@"}");
             return sb.ToString();
         }
     }
